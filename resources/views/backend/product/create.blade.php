556508c1d9e1e89e97c618dbe0ebfb9483f4eeac
HoangNH <hoangnhonline@gmail.com>
2017-07-11 08:46:24 +0700

    update

--- resources/views/backend/product/create.blade.php
+++ resources/views/backend/product/create.blade.php
@@ -42,34 +42,34 @@
 
                   <!-- Nav tabs -->
                   <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Thông tin chi tiết</a></li>
-                    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Hình ảnh</a></li>
+                    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Information</a></li>
+                    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Images</a></li>
                   </ul>
 
                   <!-- Tab panes -->
                   <div class="tab-content">
                     <div role="tabpanel" class="tab-pane active" id="home">
                         <div class="form-group col-md-6 none-padding">
-                          <label for="email">Danh mục cha<span class="red-star">*</span></label>
+                          <label for="email">Type<span class="red-star">*</span></label>
                           <select class="form-control" name="loai_id" id="loai_id">
-                            <option value="">--Chọn--</option>
+                            <option value="">--Select--</option>
                             @foreach( $loaiSpArr as $value )
                             <option value="{{ $value->id }}" {{ $value->id == old('loai_id') || $value->id == $loai_id ? "selected" : "" }}>{{ $value->name }}</option>
                             @endforeach
                           </select>
                         </div>
                           <div class="form-group col-md-6 none-padding pleft-5">
-                          <label for="email">Danh mục con</label>
+                          <label for="email">Category</label>
 
                           <select class="form-control" name="cate_id" id="cate_id">
-                            <option value="">--Chọn--</option>
+                            <option value="">--Select--</option>
                             @foreach( $cateArr as $value )
                             <option value="{{ $value->id }}" {{ $value->id == old('cate_id') || $value->id == $cate_id ? "selected" : "" }}>{{ $value->name }}</option>
                             @endforeach
                           </select>
                         </div>  
                         <div class="form-group col-md-6 none-padding" >                  
-                          <label>Tên <span class="red-star">*</span></label>
+                          <label>Name <span class="red-star">*</span></label>
                           <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
                         </div>
                         <div class="form-group col-md-6 pleft-5">                  
@@ -100,12 +100,36 @@
                           <label>Requirements</label>                  
                           <input type="text" class="form-control" name="slug" id="slug" value="{{ old('slug') }}">
                         </div>
-                        <!--
-                        <div class="col-md-4 none-padding">
+                        <div class="form-group col-md-6 none-padding" >                  
+                          <label>Số sao</label>
+                          <select class="form-control" name="no_star" id="no_star">
+                            <option value="">--Select--</option>
+                            <option value="3">3</option>
+                            <option value="3.5">3.5</option>
+                            <option value="4">4</option>
+                            <option value="4.5">4.5</option>
+                            <option value="5">5</option>
+                          </select>
+                        </div>
+                        <div class="form-group col-md-6 pleft-5">                  
+                          <label>Author</label>                  
+                          <input type="text" class="form-control" name="author" id="author" value="{{ old('author') }}">
+                        </div>
+                        <div class="form-group col-md-6 none-padding" >                  
+                          <label>Lastest version</label>
+                          <input type="text" class="form-control" name="latest_version" id="latest_version" value="{{ old('latest_version') }}">
+                        </div>
+                        <div class="form-group col-md-6 pleft-5">                  
+                          <label>Publish date</label>                  
+                          <input type="text" class="form-control" name="publish_date" id="publish_date" value="{{ old('publish_date') }}">
+                        </div>
+                        
+                        <div class="col-md-12">
                           <div class="checkbox">
-                              <label><input type="checkbox" name="is_hot" value="1"> Game HOT </label>
+                              <label><input type="checkbox" name="is_hot" value="1"> <span style="color:blue">Game HOT </span></label>
                           </div>                          
                         </div>
+                        <!--
                         <div class="col-md-4 none-padding pleft-5">
                             <div class="checkbox">
                               <label><input type="checkbox" name="is_sale" value="1"> Game SALE </label>
@@ -133,13 +157,13 @@
                             <input type="text" class="form-control" name="video_url" id="video_url" value="{{ old('video_url') }}">
                         </div>-->
                         <div class="form-group">
-                            <label>Mô tả ngắn</label>
+                            <label>Description</label>
                             <textarea class="form-control" rows="4" name="description" id="description">{{ old('description') }}</textarea>
                           </div>
                         
                          
                         <div class="form-group">
-                          <label>Chi tiết</label>
+                          <label>Detail</label>
                           <textarea class="form-control" rows="10" name="content" id="content">{{ old('content') }}</textarea>
                         </div>
                     </div><!--end thong tin co ban-->
@@ -178,7 +202,7 @@
       <div class="col-md-4">      
         <div class="box box-primary">
           <div class="box-header with-border">
-            <h3 class="box-title">Thông tin SEO</h3>
+            <h3 class="box-title">SEO Information</h3>
           </div>
 
           <!-- /.box-header -->
@@ -243,12 +267,12 @@ $(document).on('click', '.remove-image', function(){
       $('#dataForm').submit(function(){
         /*var no_cate = $('input[name="category_id[]"]:checked').length;
         if( no_cate == 0){
-          swal("Lỗi!", "Chọn ít nhất 1 thể loại!", "error");
+          swal("Lỗi!", "Select ít nhất 1 thể loại!", "error");
           return false;
         }
         var no_country = $('input[name="country_id[]"]:checked').length;
         if( no_country == 0){
-          swal("Lỗi!", "Chọn ít nhất 1 quốc gia!", "error");
+          swal("Lỗi!", "Select ít nhất 1 quốc gia!", "error");
           return false;
         }        
         */
