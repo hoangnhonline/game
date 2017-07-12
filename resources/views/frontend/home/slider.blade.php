6ac49e7b1eeeb78c7cbdc0f0d62296ea0125fed3
HoangNH <hoangnh@evolableasia.vn>
2017-07-12 08:51:44 +0700

    update

--- resources/views/frontend/home/slider.blade.php
+++ resources/views/frontend/home/slider.blade.php
@@ -6,31 +6,17 @@ $bannerArr = DB::table('banner')->where(['object_id' => 1, 'object_type' => 3])-
 ?>
 @if($bannerArr)
 <div class="block block_slide">
-	<div class="owl-carousel dotsData" data-nav="true" data-margin="0" data-items='1' data-autoplayTimeout="700" data-autoplay="false" data-loop="true" data-dots="true">
-		<div class="item-slide" data-dot="1">
-			<img src="{{ URL::asset('assets/images/slide/banner.jpg') }}" alt="banner">
-			<div class="figcaption">
-				プロジェクト東京ドールズ-アラームアプリ-
-			</div>
+	<div class="owl-carousel dotsData" data-nav="true" data-margin="0" data-items='1' data-autoplayTimeout="700" data-autoplay="true" data-loop="true" data-dots="true">
+		<?php $i = 0; ?>
+		@foreach($bannerArr as $value)
+		<?php $i++; ?>
+		<div class="item-slide" data-dot="{{ $i }}">
+			<img src="{{ Helper::showImage($value->image_url) }}" alt="banner">
+			<!--<div class="figcaption">
+				
+			</div>-->
 		</div><!-- item-slide -->
-		<div class="item-slide" data-dot="2">
-			<img src="{{ URL::asset('assets/images/slide/banner1.jpg') }}" alt="banner">
-			<div class="figcaption">
-				DEAD TARGET: Zombie
-			</div>
-		</div><!-- item-slide -->
-		<div class="item-slide" data-dot="3">
-			<img src="{{ URL::asset('assets/images/slide/banner2.jpg') }}" alt="banner">
-			<div class="figcaption">
-				The Walking Dead No Man's Land
-			</div>
-		</div><!-- item-slide -->
-		<div class="item-slide" data-dot="4">
-			<img src="{{ URL::asset('assets/images/slide/banner3.jpg') }}" alt="banner">
-			<div class="figcaption">
-				Golden Arcana: Tactics (Unreleased)
-			</div>
-		</div><!-- item-slide -->
+		@endforeach
 	</div>
 </div><!-- /block_slide -->
 @endif
